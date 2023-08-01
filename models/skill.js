ba65331d67cad45a50feee71ee8b8d270612ae6b
lsmith <therealshaunsred@gmail.com>
2023-08-01 15:20:03 -0400

    updated models/skill.js, added getOne and getAll functions

---
+++ models/skill.js
@@ -0,0 +1,20 @@
+const Skills = [
+    {skill: 'General', proficiencies: ['Using git/github', 'Using MVC architechture', 'Working as part of a dev team']},
+    {skill: 'CSS', proficiencies: ['Creating Animations','Customizing HTML Layouts','Styling HTML webpages', 'Using CSS frameworks']},
+    {skill: 'JavaScript', proficiencies: ['DOM manipulation','Working with asynchronous functions','Object-oriented design']},
+    {skill: 'HTML', proficiencies: ['Creating dynamic, responsive webpages', 'Using symantic elements'] },
+    {skill: 'Node.js', proficiencies: ['Using Express to handle routing']}
+]
+
+module.exports = {
+    getAll,
+    getOne,
+}
+
+function getAll() {
+    return Skills;
+}
+
+function getOne(idx){
+    return Skills[idx];
+}
